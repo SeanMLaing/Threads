8240746f43268727ddd2a8a73e2432f5338f6655
Sean <sean.laing@protonmail.com>
2023-05-24 19:48:27 -0600

    Base layout

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Threads
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Starting Threads...");
+
+            // Global Variables
+            bool loading = true;
+            int whileCount = 0;
+
+            Thread calculation = new Thread(() =>
+            {
+                // Do the calculations
+
+
+            });
+
+            calculation.Start();
+
+            while (loading)
+            {
+                Console.Write($"Loading");
+                
+                // Check if count divisible by 3, remainder + 1 is how many '.' to show after 'Loading'
+                int remainder = whileCount % 3;
+                
+                switch (remainder + 1)
+                {
+                    case 1:
+                    {
+
+                        break;
+                    }
+                    case 2:
+                    {
+
+                        break;
+                    }
+                    case 3:
+                    {
+
+                        break;
+                    }
+                    default:
+                    {
+                            throw new Exception("How did we get here!?");
+                        break;
+                    }
+                }
+                
+
+
+
+            }
+            
+
+
+            Console.WriteLine($"Program Complete...");
+        }
+
+
+
+
+
+
+
+    }
+}
