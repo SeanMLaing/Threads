823c9ff9ee2d5536828465ce461feb93ed3c6f89
Sean <sean.laing@protonmail.com>
2023-05-24 21:17:30 -0600

    Hash a random number 20 million times to show the loading message

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Threads
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Threads
 {
     public class Program
     {
@@ -10,18 +13,43 @@
             bool loading = true;
             int whileCount = 0;
 
+            double resultNum = 0;
+
+
+            Console.WriteLine($"Our objective is to run some cumputationally heavy functions and to show a loading message using different threads");
+            DateTime start = DateTime.Now;
+            DateTime end = DateTime.Now;
+
             Thread calculation = new Thread(() =>
             {
-                // Do the calculations
+                start = DateTime.Now;
+                for (int i = 0; i < 200000000; i++)
+                {
+                    // Do the calculations 
+                    using (SHA256 mySHA256 = SHA256.Create())
+                    {
+                        Random random = new Random(1000000000);
 
+                        // hash above num
+                        byte[] bytes = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(random.ToString()));
+                        //Console.WriteLine("Hashed 1");
 
+                    }
+                }
+                end = DateTime.Now;
             });
 
             calculation.Start();
 
             while (loading)
             {
-                Console.Write($"Loading");
+                if (!calculation.IsAlive)
+                {
+                    calculation.Join();
+                    loading = false;
+                }
+
+                Console.Write($"\nLoading");
                 
                 // Check if count divisible by 3, remainder + 1 is how many '.' to show after 'Loading'
                 int remainder = whileCount % 3;
@@ -30,39 +58,46 @@
                 {
                     case 1:
                     {
-
+                        Console.Write(".");
                         break;
                     }
                     case 2:
                     {
-
+                        Console.Write("..");
                         break;
                     }
                     case 3:
                     {
-
+                        Console.Write("...");
                         break;
                     }
                     default:
                     {
-                            throw new Exception("How did we get here!?");
+                        throw new Exception("How did we get here!?");
                         break;
                     }
                 }
-                
 
 
 
+                whileCount++;
+                Thread.Sleep(250);
             }
+
+
             
 
+            Console.WriteLine($"Thread has completed, time taken: {end - start}");
 
             Console.WriteLine($"Program Complete...");
         }
 
 
 
-
+        public static double Calc(int i)
+        {
+            return 1 + i / (2.0 * i + 1) * Calc(i + 1);
+        }
 
 
 
